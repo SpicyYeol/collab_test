57c453b298a355e5b25fcba4c2aa2f317dfec908
Dae-Yeol Kim <spicyyeol@gmail.com>
2026-05-06 09:36:52 +0900

    feat: input's and func call

---
+++ main.py
@@ -0,0 +1,8 @@
+from ultils import add, sub, mul
+a = int(input("insert a: "))
+b = int(input("insert b: "))
+str = input("insert str:")
+
+print("add", add(a,b))
+print("sub", sub(a,b))
+print("mul", mul(a,b))
